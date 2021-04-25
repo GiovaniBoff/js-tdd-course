a04ff84e35e5e0bdc2966c70dfee2229bb7aae9e
Giovani Boff <giovanisouza15@gmail.com>
2021-04-25 18:16:48 -0300

    add code coverage

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -19,7 +19,10 @@ describe('Main', () => {
     expect(FizzBuzz(30)).to.be.equal('FizzBuzz');
   });
 
-  it('shoul return the number when non-multiple', () => {
+  it('should return the number when non-multiple', () => {
     expect(FizzBuzz(7)).to.be.equal(7);
+  });
+  it('should return 0 when the namber is 0', () => {
+    expect(FizzBuzz(0)).to.be.equal(0);
   });
 });
